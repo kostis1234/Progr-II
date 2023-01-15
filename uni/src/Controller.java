5cfd54295e83ea759b82f7522b551165e4e73211
MariannaDima <117856133+MariannaDima@users.noreply.github.com>
2023-01-15 11:01:18 +0200

    Update Controller.java

--- uni/src/Controller.java
+++ uni/src/Controller.java
@@ -8,46 +8,47 @@ public class Controller {
     public static SendMessageImpl sendMessage = new SendMessageImpl();
     public static ReadMessageImpl readMessage = new ReadMessageImpl();
 
-
     public static void main(String[] args) throws IOException {
         System.out.println("Welcome to our app!");
-        System.out.println("For login type Yes");
-        Scanner obj = new Scanner(System.in);
-        String answer = obj.nextLine();
-        if (Objects.equals(answer, "Yes")) {
-            System.out.println("For login press 1");
-            System.out.println("For register press 2");
-            Scanner obj2 = new Scanner(System.in);
-            int answer2 = obj2.nextInt();
-            boolean success = false;
-            String username = null;
-            if (answer2 == 1) {
-                 success = login.login();
-                 username = LoginImpl.getName();
-            } else if (answer2 == 2) {
-                 success =  registerService.register();
-                 username = RegisterServiceImpl.getName();
+        Scanner obj2 = new Scanner(System.in);
+        boolean success = false;
+        String username = null;
+        boolean done = true;
+        while (done) {
+            System.out.println("*To login type 1" +
+                    "\n*To register type 2");
+            String answer2 = obj2.nextLine();
+            if (Objects.equals(answer2, "1")) {
+                success = login.login();
+                username = LoginImpl.getName();
+                done = false;
+            } else if (Objects.equals(answer2, "2")) {
+                success = registerService.register();
+                username = RegisterServiceImpl.getName();
+                done = false;
+            } else {
+                System.out.println("Non existing answer;try again!");
             }
-            if(success) {
+        }
+        if (success) {
+            System.out.println("To logout type 3");
+            System.out.println("To read messages type 4");
+            System.out.println("To send messages type 5");
+            String finalAnswer = obj2.nextLine();
+            while (!Objects.equals(finalAnswer, "3")) {
+                if (Objects.equals(finalAnswer, "4")) {
+                    readMessage.readMessage(username);
+                } else if (Objects.equals(finalAnswer, "5")) {
+                    sendMessage.sendMessages(username);
+                } else {
+                    System.out.println("Non existing answer;try again!");
+                }
                 System.out.println("To logout type 3");
                 System.out.println("To read messages type 4");
                 System.out.println("To send messages type 5");
-                int finalAnswer = obj2.nextInt();
-                while(finalAnswer !=3 ) {
-                    if(finalAnswer == 4){
-                        readMessage.readMessage(username);
-                    } else if (finalAnswer == 5) {
-                        sendMessage.sendMessages(username);
-                    }
-                    System.out.println("For logout press 3");
-                    System.out.println("For reading messages type 4");
-                    System.out.println("For sending messages type 5");
-                    finalAnswer = obj2.nextInt();
-                }
+                finalAnswer = obj2.nextLine();
             }
-
-        } else {
-            System.out.println("Thanks for your time!");
         }
+        System.out.println("Thank you for your time!");
     }
 }
