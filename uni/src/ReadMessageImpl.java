aa2f4c17c5c7018f9994f68132525c8dc8faccaa
MariannaDima <117856133+MariannaDima@users.noreply.github.com>
2023-01-15 11:04:54 +0200

    Update ReadMessageImpl.java

--- uni/src/ReadMessageImpl.java
+++ uni/src/ReadMessageImpl.java
@@ -1,16 +1,16 @@
 import java.io.*;
-import java.util.Objects;
 import java.util.Scanner;
 
-public class ReadMessageImpl implements ReadMessageService{
+public class ReadMessageImpl implements ReadMessageService {
     @Override
     public void readMessage(String name) {
         BufferedReader reader;
         try {
-            reader = new BufferedReader(new FileReader(name +".txt"));
+            //Opening the correct txt file via the given username.
+            reader = new BufferedReader(new FileReader(name + ".txt"));
             String line = reader.readLine();
-            FileWriter myWriter = new FileWriter(name +".txt");
-            if(line == null) {
+            FileWriter myWriter = new FileWriter(name + ".txt");
+            if (line == null) {
                 System.out.println("No messages!");
             }
             while (line != null) {
@@ -18,11 +18,13 @@ public class ReadMessageImpl implements ReadMessageService{
                 System.out.println("Do you want to like the message?Yes/No");
                 Scanner obj = new Scanner(System.in);
                 String answer = obj.nextLine();
-                if (Objects.equals(answer, "Yes")) {
+                //Ignoring caps case.
+                if (answer.equalsIgnoreCase("Yes")) {
                     like(line, name);
                 }
                 line = reader.readLine();
             }
+            //Delete the messages when the user reads them.
             myWriter.flush();
             reader.close();
 
@@ -30,10 +32,13 @@ public class ReadMessageImpl implements ReadMessageService{
             e.printStackTrace();
         }
     }
+
     public void like(String message, String name) throws IOException {
+        //Splitting the line of the message and taking the last word which is the username of the user who send it.
         String[] parts = message.split(" ");
         String lastWord = parts[parts.length - 1];
-        FileWriter fw = new FileWriter(lastWord +".txt", true);
+        //Opening the sender's txt file and sending the message of the like.
+        FileWriter fw = new FileWriter(lastWord + ".txt", true);
         BufferedWriter bw = new BufferedWriter(fw);
         PrintWriter out = new PrintWriter(bw);
         out.println(name + " liked your message!");
