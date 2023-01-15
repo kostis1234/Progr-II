ead8d7f4c811ea7c7046ac7ecc4ec3126abc12af
MariannaDima <117856133+MariannaDima@users.noreply.github.com>
2023-01-15 11:02:03 +0200

    Update CreationOfFiles.java

--- uni/src/CreationOfFiles.java
+++ uni/src/CreationOfFiles.java
@@ -2,22 +2,23 @@ import java.io.File;
 import java.io.IOException;
 
 public class CreationOfFiles {
+    //This class is used only one time to create two txt files!
     public static void main(String[] args) {
         try{
-            File myObj = new File("usernames.txt");
-            if (myObj.createNewFile()) {
-                System.out.println("File created: " + myObj.getName());
+            File file = new File("usernames.txt");
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
             }
         } catch (IOException e) {
-            System.out.println("Something went wrong!");
+            System.out.println("The creation did not occurred!");
         }
         try{
-            File myObj = new File("passwords.txt");
-            if (myObj.createNewFile()) {
-                System.out.println("File created: " + myObj.getName());
+            File obj = new File("passwords.txt");
+            if (obj.createNewFile()) {
+                System.out.println("File created: " + obj.getName());
             }
         } catch (IOException e) {
-            System.out.println("Something went wrong!");
+            System.out.println("The creation did not occurred!");
         }
     }
 }
