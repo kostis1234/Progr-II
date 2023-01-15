4ad1ade98b23f9574297a23a17411dd8544ec6c1
MariannaDima <117856133+MariannaDima@users.noreply.github.com>
2023-01-15 11:05:55 +0200

    Update RegisterServiceImpl.java

--- uni/src/RegisterServiceImpl.java
+++ uni/src/RegisterServiceImpl.java
@@ -3,53 +3,67 @@ import java.util.Scanner;
 
 public class RegisterServiceImpl implements RegisterService {
     protected static String name;
+
     public static String getName() {
         return name;
     }
+
     @Override
     public boolean register() throws IOException {
-        boolean success;
+        //Success being used as a flag;if the user logged in successfully or not.
+        boolean success = false;
         Scanner obj = new Scanner(System.in);
         System.out.println("Enter a username");
-         name = (String) obj.nextLine();
-        System.out.println("Enter a password");
-        String password = (String) obj.nextLine();
-        File file = new File("usernames.txt");
-        if (countWord(name) != 0) {
-            System.out.println("This username is in use!");
-            success = false;
-        } else {
-            FileWriter fw = new FileWriter("usernames.txt", true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter out = new PrintWriter(bw);
-            FileWriter fw1 = new FileWriter("passwords.txt", true);
-            BufferedWriter bw1 = new BufferedWriter(fw1);
-            PrintWriter out1 = new PrintWriter(bw1);
-            out.println(name);
-            out1.println(password);
-            File myObj = new File(name +".txt");
-            myObj.createNewFile();
-
-            out1.close();
-            out.close();
-            success = true;
+        name =  obj.nextLine();
+        boolean end = true;
+        //This loop allows the user to try multiple usernames until the success sign up.
+        while(end) {
+            //Checking if the username already exists.
+            if (countWord(name) != 0) {
+                System.out.println("Sorry this username is in use!");
+                Scanner obj2 = new Scanner(System.in);
+                System.out.println("Do you want to try again? *Type Yes*");
+                String answer = obj2.nextLine();
+                if(answer.equalsIgnoreCase("Yes")){
+                    System.out.println("Enter a username");
+                    name =  obj.nextLine();
+                }else {
+                    end = false;
+                }
+            } else {
+                System.out.println("Enter a password");
+                String password = obj.nextLine();
+                //Adding the password and the username to txt files.
+                FileWriter username = new FileWriter("usernames.txt", true);
+                BufferedWriter writer = new BufferedWriter(username);
+                PrintWriter print = new PrintWriter(writer);
+                FileWriter pasSword = new FileWriter("passwords.txt", true);
+                BufferedWriter writer1 = new BufferedWriter(pasSword);
+                PrintWriter print1 = new PrintWriter(writer1);
+                print.println(name);
+                print1.println(password);
+                //Creating a unique txt file naming it as the username.
+                File myObj = new File(name + ".txt");
+                myObj.createNewFile();
+                print.close();
+                print1.close();
+                success = true;
+                end = false;
+            }
         }
         return success;
     }
 
     public static int countWord(String word) throws FileNotFoundException {
         int flag = 0;
+        // Flag works as a flag.If it is 0 it means that the username does not exist.
         Scanner scanner = new Scanner(new File("usernames.txt"));
         while (scanner.hasNext()) {
             String aString = scanner.nextLine();
-            if (aString.equalsIgnoreCase(word)) {
+            if (aString.equals(word)) {
                 flag = 1;
             }
         }
         return flag;
     }
-
 }
-
-
-
